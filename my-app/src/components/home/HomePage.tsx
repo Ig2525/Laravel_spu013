b5a2cf89523028698a9842c2355ae1b5cea1f9ac
Ig2525 <ig25@ukr.net>
2023-01-21 13:37:21 +0200

    .gitignore Fixed

---
+++ my-app/src/components/home/HomePage.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import http from "../../http_common";
+import Pagination from "../common/Pagination";
+import { GetProductAction, IProductItem, IProductResponse, IProductState, ProductActionTypes } from "./types";
+const HomePage = () => {
+  //const [list, setList] = useState<Array<IProductItem>>([]);
+  const { list, total, count_page, current_page } = useSelector((state: any) => state.product as IProductState);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    http.get<IProductResponse>("/api/products").then((resp) => {
+      console.log("List product server", resp);
+      const {data} = resp;
+
+      const payload: IProductState = {
+        list: data.data,
+        count_page: data.last_page,
+        current_page: data.current_page,
+        total: data.total
+      };
+
+      const action: GetProductAction = {
+        type: ProductActionTypes.GET_PRODUCTS,
+        payload: payload
+      };
+
+      dispatch(action);
+    });
+  }, []);
+
+  const data = list.map((product) => (
+    <tr key={product.id}>
+      <td>{product.id}</td>
+      <td>{product.name}</td>
+      <td>{product.detail}</td>
+    </tr>
+  ));
+
+  const handleClickPage = (page:number) => {
+    console.log("Click, number", page);
+  }
+
+  return (
+    <>
+      <h1 className="text-center">Головна сторінка</h1>
+      <h4>Всього записів <strong>{total}</strong></h4>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Detail</th>
+          </tr>
+        </thead>
+        <tbody>{data}</tbody>
+      </table>
+      
+
+      <Pagination 
+      count_page={count_page} 
+      current_page={current_page} 
+      onClick={handClickPage}
+      />
+    </>
+  );
+};
+
+export default HomePage;
